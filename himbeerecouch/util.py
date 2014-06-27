def605ea49035fda76a7428f1939bc51c20f0049
Michael Marino <mmarino@gmail.com>
2014-06-27 17:25:09 +0200

    Add ability to blink LEDs

--- himbeerecouch/util.py
+++ himbeerecouch/util.py
@@ -8,6 +8,7 @@
 import sys, os, time, atexit
 from signal import SIGTERM 
 import hashlib, uuid
+import threading 
 
 class Daemon:
 	"""
@@ -163,4 +164,39 @@ def getpassword():
       Get password, which is a combination of mac id and serial number
     """
     return str(hashlib.sha1(str(getmacid()) + getserial()).hexdigest())
+
+def blink_leds():
+    """
+      Blink the leds on the RPi
+    """
+    try:
+        import RPs.GPIO as GPIO
+    except:
+        raise Exception("Only available ON Raspberry Pi")
+
+    def _f(t):
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        # set up GPIO output channel
+        GPIO.setup(16, GPIO.OUT)
+        
+        while t.is_set():
+            GPIO.output(16, GPIO.LOW)
+            sleep(0.4)
+            GPIO.output(16, GPIO.HIGH)
+            sleep(0.4)
+
+    o = threading.Event()
+    o.set()
+    t = threading.Thread(target=_f, args=(o,))
+    t.start()
+    return t, o
+      
+def stop_blinking(v):
+    """
+      stop blinking the leds
+    """
+    t, o = v 
+    o.clear()
+    t.join()
 
