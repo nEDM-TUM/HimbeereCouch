07f465d38f7cb847df603ef9e9babe9451ae1c20
Michael Marino <mmarino@gmail.com>
2014-07-01 14:00:56 +0200

    Allow executing commands in the receive_broadcast_message

--- himbeerecouch/prog.py
+++ himbeerecouch/prog.py
@@ -217,23 +217,52 @@ def receive_broadcast_message(timeout=1000):
     """
       Receives broadcast message and returns MAC id and password
     """
+    import subprocess as _sp
     v = blink_leds() 
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind(('<broadcast>', _broadcast_port))
-    s.settimeout(timeout)
+    total_timeout = timeout
+    s.settimeout(1.0)
 
-    def _handler(*args):
-       pass
-
-    oh = signal.signal(signal.SIGTERM, _handler)
-    
+    oh = None
     try:
-        msg, addr = s.recvfrom(1024)
-        dic = json.loads(msg)
-        s.sendto(json.dumps(dict(MacID=getmacid(),password=getpassword())), addr)
-        return dic['server']
-    except:
-        return None
-    finally:
-        stop_blinking(v)
-        signal.signal(signal.SIGTERM, oh)
+        # Handle SIGTERM if we are on the main thread
+        def _handler(*args):
+           pass
+        oh = signal.signal(signal.SIGTERM, _handler)
+    except: pass
+    
+    log("Wait for broadcast...")
+    while 1:
+        try:
+            msg, addr = s.recvfrom(1024)
+            dic = json.loads(msg)
+            if "server" in dic:
+                s.sendto(json.dumps(dict(MacID=getmacid(),password=getpassword())), addr)
+                log("Received...")
+                return dic['server']
+            elif all(k in dic for k in ("MacID", "password", "cmd")): 
+                if dic["MacID"] == getmacid() and \
+                   dic["password"] == getpassword():
+                    # Means this is for me
+                    log("Running cmd: " + str(dic["cmd"]))
+                    try:
+                        p = _sp.Popen(dic["cmd"], stderr=_sp.PIPE, stdout=_sp.PIPE)
+                        dic["ret"] = p.communicate()
+                    except Exception as e:
+                        dic["ret"] = (None,repr(e))
+                    finally:
+                        s.sendto(json.dumps(dic), addr) 
+        except Exception as e:
+            if e.__class__ == socket.timeout:
+                if _listen_should_quit: return None
+                if timeout <= 0: continue 
+                total_timeout -= 1.0
+                if total_timeout > 0: continue
+                log("Timed out...")
+            else:
+                log("Exception: " + repr(e))
+            return None
+        finally:
+            stop_blinking(v)
+            if oh: signal.signal(signal.SIGTERM, oh)
