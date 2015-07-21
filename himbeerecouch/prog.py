fde9c950af2f4ee1bf065b19ab1da60454672752
Michael Marino <mmarino@gmail.com>
2015-07-21 12:58:10 +0200

    Only check if process is not alive.

--- himbeerecouch/prog.py
+++ himbeerecouch/prog.py
@@ -132,14 +132,15 @@ class RaspberryDaemon(Daemon):
             for anid,x in processes.items():
                 t,q = x
                 try:
-                    log("Checking output")
-                    res = q.get(False)
-                    log("Got output")
-                    t.join()
-                    if "ok" not in res:
-                        log("Error seen ({}) : {}".format(anid, res["error"]))
-                    log("Will remove process")
-                    del_list.append(anid)
+                    if not t.is_alive():
+                        log("Checking output")
+                        res = q.get(False)
+                        log("Got output")
+                        t.join()
+                        if "ok" not in res:
+                            log("Error seen ({}) : {}".format(anid, res["error"]))
+                        log("Will remove process")
+                        del_list.append(anid)
                 except (Queue.Empty,IOError):
                     pass
                 except:
